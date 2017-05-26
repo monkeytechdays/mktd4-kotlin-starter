c3c749d18c7648b0cad58adb326bb4f7509be1ea
Igor Laborie <ilaborie@gmail.com>
2017-05-26 11:03:19 +0200

    Ex3 fix

--- ex3/src/test/kotlin/Ex3Task.kt
+++ ex3/src/test/kotlin/Ex3Task.kt
@@ -3,28 +3,31 @@ import util.Player
 /**
  * Exercise 3 : Nullable & Safe Cast.
  *
- * Read About @see <a href="http://kotlinlang.org/docs/reference/null-safety.html"></a> and
+ * Read About @see <a href="http://kotlinlang.org/docs/reference/null-safety.html">Null Safety</a>
  */
 
 /**
- * return the player score or 0 using one line syntax and if operator
+ * return the player score or 0 (if no player or no score) using some if operator
  */
 fun getPlayerScoreUsingIf(player: Player?): Int = TODO()
 
 
 /**
- * return the player score or 0 using one line syntax and elvis operator
+ * return the player score or 0 (if no player or no score) using one line syntax and elvis operator (no if here)
  */
 fun getPlayerScoreUsingElvis(player: Player?): Int = TODO()
 
 
 /**
+ * Maybe your convinced that the player exists, and he have a score.
  * return the player score.
+ * Note: this is dangerous !
  */
 fun trustMeAndReturnScore(player: Player?): Int = TODO()
 
 /**
  * Cast the @param arg to Int and return its value or 0 if it's not an Int
+ * Read about @see <a href="https://kotlinlang.org/docs/reference/typecasts.html">Type Checks and Casts</a>
  */
 fun anyToInt(arg: Any?): Int = TODO()
 
