ddce59220d4b8c561f1cb3a28e87812f3ac89c1c
Igor Laborie <ilaborie@gmail.com>
2017-05-26 11:47:58 +0200

    Ex7 clarification

--- ex7/src/test/kotlin/Ex7Task.kt
+++ ex7/src/test/kotlin/Ex7Task.kt
@@ -3,12 +3,13 @@ package io.monkeypatch.mktd4.ex7
 /**
  * Exercise 7: Lambdas
  *
- * Read about <a href="http://kotlinlang.org/docs/reference/lambdas.html"></a>
+ * Read about <a href="http://kotlinlang.org/docs/reference/lambdas.html">Higher-Order Functions and Lambdas</a>
  */
 
 
 /**
- * Pass a lambda to any function to check if the collection contains an even number. The function any gets a predicate as an argument and returns true if there is at least one element satisfying the predicate.
+ * Pass a lambda to any function to check if the collection contains an even number. 
+ * The function `any` gets a predicate as an argument and returns true if there is at least one element satisfying the predicate.
  */
 fun containsEven(collection: Collection<Int>): Boolean = collection.any {
     /*TODO*/
